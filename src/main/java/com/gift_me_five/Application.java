6a727880d28fa08c17e19701229d607d007907c7
Steffen Klaski <steffen.klaski@t-systems.com>
2019-12-16 11:10:26 +0100

    Maven created

---
+++ src/main/java/com/gift_me_five/Application.java
@@ -0,0 +1,13 @@
+package com.gift_me_five;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
