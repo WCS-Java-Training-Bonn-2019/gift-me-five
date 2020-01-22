023ff5e7fd3bc362a79bfcb03b4129c7a06e26fc
Steffen Klaski <steffen.klaski@t-systems.com>
2020-01-22 18:33:41 +0100

    ManyToMany established (hopefully)

--- src/main/java/com/gift_me_five/entity/Wishlist.java
+++ src/main/java/com/gift_me_five/entity/Wishlist.java
@@ -1,12 +1,16 @@
 package com.gift_me_five.entity;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -22,11 +26,14 @@ public class Wishlist {
     @ManyToOne
     @JoinColumn(name = "receiverId", nullable=false)
     private  User user;
-    
-    
+        
     @ManyToOne
     @JoinColumn(name = "themeId")
     private  Theme theme;
+    
+    @ManyToMany
+    @JoinTable(name="GiverSeeWishlist", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns=@JoinColumn(name="wishlist_id"))
+    private List<User> users = new ArrayList<>();
     
 	public Wishlist() {
 	}
@@ -69,6 +76,62 @@ public class Wishlist {
 
 	public void setModify_date(Date modify_date) {
 		this.modifyDate = modify_date;
+	}
+
+	public String getUniqueUrlGiver() {
+		return uniqueUrlGiver;
+	}
+
+	public void setUniqueUrlGiver(String uniqueUrlGiver) {
+		this.uniqueUrlGiver = uniqueUrlGiver;
+	}
+
+	public String getUniqueUrlReceiver() {
+		return uniqueUrlReceiver;
+	}
+
+	public void setUniqueUrlReceiver(String uniqueUrlReceiver) {
+		this.uniqueUrlReceiver = uniqueUrlReceiver;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Theme getTheme() {
+		return theme;
+	}
+
+	public void setTheme(Theme theme) {
+		this.theme = theme;
+	}
+
+	public List<Wish> getWishs() {
+		return wishs;
+	}
+
+	public void setWishs(List<Wish> wishs) {
+		this.wishs = wishs;
 	}
 	
 }
