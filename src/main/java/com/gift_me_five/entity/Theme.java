3d1b0e1b1df0a94fceba89ee8deca359c9e58d92
Steffen Klaski <steffen.klaski@t-systems.com>
2020-01-23 11:36:55 +0100

    more sql adjustments

--- src/main/java/com/gift_me_five/entity/Theme.java
+++ src/main/java/com/gift_me_five/entity/Theme.java
@@ -1,6 +1,6 @@
 package com.gift_me_five.entity;
 
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
--- src/main/java/com/gift_me_five/entity/Wishlist.java
+++ src/main/java/com/gift_me_five/entity/Wishlist.java
@@ -1,6 +1,6 @@
 package com.gift_me_five.entity;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 
