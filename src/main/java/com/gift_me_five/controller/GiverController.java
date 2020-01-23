834b09cef757675c291dc9ee51c74c0f625f638d
Steffen Klaski <steffen.klaski@t-systems.com>
2020-01-23 15:34:29 +0100

    giver: thymeleaf for list

---
+++ src/main/java/com/gift_me_five/controller/GiverController.java
@@ -0,0 +1,22 @@
+package com.gift_me_five.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.gift_me_five.repository.WishRepository;
+
+@Controller
+public class GiverController {
+	
+	@Autowired
+	private WishRepository repository;
+		
+	@GetMapping("/giver")
+	public String getAll(Model model) {
+		model.addAttribute("wishes", repository.findAll());
+		System.out.println(repository.findAll());
+		return "giver";
+	}
+
+}
