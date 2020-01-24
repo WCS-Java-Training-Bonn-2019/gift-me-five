3291f2955bfc603fb4054c950f7c6a3437041c27
Arne Chrestin <arne.chrestin@telekom.de>
2020-01-24 10:42:11 +0100

    Changes to giver logic

--- src/main/java/com/gift_me_five/controller/GiverController.java
+++ src/main/java/com/gift_me_five/controller/GiverController.java
@@ -7,24 +7,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.gift_me_five.entity.User;
 import com.gift_me_five.entity.Wish;
+import com.gift_me_five.repository.UserRepository;
 import com.gift_me_five.repository.WishRepository;
+import com.gift_me_five.repository.WishlistRepository;
 
 @Controller
 public class GiverController {
-	
+
 	@Autowired
 	private WishRepository repository;
-		
+
+	@Autowired
+	private WishlistRepository wlRepository;
+
+	@Autowired
+	private UserRepository uRepository;
+
 	@GetMapping("/giver")
 	public String getAll(Model model) {
-		model.addAttribute("wishes", repository.findAll());		
+		model.addAttribute("wishes", repository.findAll());
 		return "giver";
 	}
-	
 
 	@PostMapping("/giver")
-	public String updateWish(@ModelAttribute Wish wish) {
+	public String updateWish(@ModelAttribute(value = "wish") Wish wish,
+			@ModelAttribute(value = "giverId") Long giverId) {
 		System.out.println();
 		System.out.println();
 		System.out.println();
@@ -35,7 +44,13 @@ public class GiverController {
 		System.out.println();
 		System.out.println();
 		System.out.println();
-		System.out.println();
+		System.out.println("Giver ID: " + giverId);
+		if (giverId == 0) {
+			wish.setGiver(null);
+		} else {
+			wish.setGiver(uRepository.findById(giverId).get());
+		}
+		wish.setWishlist(wlRepository.findById(1L).get());
 		System.out.println(repository.save(wish));
 		return "redirect:/giver";
 	}
