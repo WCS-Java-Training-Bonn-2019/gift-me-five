222ba89c7dda1a674b500bdc58a6ba61bd0caa9c
Steffen Klaski <steffen.klaski@t-systems.com>
2020-01-30 11:16:02 +0100

    admin pages userhandling finished

--- src/main/java/com/gift_me_five/controller/adminController.java
+++ src/main/java/com/gift_me_five/controller/adminController.java
@@ -1,10 +1,18 @@
 package com.gift_me_five.controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.gift_me_five.entity.User;
 import com.gift_me_five.repository.UserRepository;
 import com.gift_me_five.repository.WishRepository;
 import com.gift_me_five.repository.WishlistRepository;
@@ -24,7 +32,35 @@ public class adminController {
 	@GetMapping("/admin/user")
 	public String getAll(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "admin/get_all_User";
+		return "/admin/get_all_User";
+	}
+	
+	@GetMapping({"/admin/new_user", "/admin/edit_user/{id}"})
+	public String editUser(Model model, @PathVariable(required = false) Long id) {
+		if (id == null) {
+			model.addAttribute("user", new User());
+			return "/admin/edit_user.html";
+		}
+		Optional<User> optionalUser = userRepository.findById(id);
+		if (optionalUser.isPresent()) {
+			model.addAttribute("user", optionalUser.get());
+		} else {
+			return "redirect:/admin/user";
+		}
+		return "/admin/edit_user";
+	}
+	
+	@PostMapping("/admin/upsert_user")
+	public String upsertuser(Model model, @Valid User user) {
+		user = userRepository.save(user);
+		return "redirect:/admin/user";
+	}
+	
+	@GetMapping("/admin/delete_user/{id}")
+	public String deleteuser(@PathVariable("id") long id) {
+		System.out.println("delete id: " + id);
+		userRepository.deleteById(id);
+		return "redirect:/admin/user";
 	}
 	
 	@GetMapping("/admin/wish")
