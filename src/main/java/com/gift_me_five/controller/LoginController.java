0721c39a98836042bead088f256cbf3c4401bec9
Michael Blisner <Michael.Blisner@telekom.de>
2020-02-06 15:33:32 +0100

    new login error

--- src/main/java/com/gift_me_five/controller/LoginController.java
+++ src/main/java/com/gift_me_five/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.gift_me_five.entity.User;
 import com.gift_me_five.service.UserArtifactsService;
@@ -15,20 +16,23 @@ public class LoginController {
 	UserArtifactsService userArtifactsService;
 	
 	@GetMapping("/")
-	public String showStartPage(Model model) {
+	public String showStartPage(Model model, @RequestParam (required = false) Long failure) {
 		
 		model.addAttribute("myWishlists", userArtifactsService.allOwnWishlists());
 		model.addAttribute("friendWishlists", userArtifactsService.allFriendWishlists());
+		model.addAttribute("failure" , failure);
 		
 		return "index";
 	}
 	
+	// route deakiviert
+	/*
 	@GetMapping("/showMyLoginPage")
 	public String showMyLoginPage(Model model) {
 		model.addAttribute("user", new User());
 		return "fancy-login";
 	}
-
+	*/
 		
 	// add request mapping for /access-denied
 	@GetMapping("/access-denied")
