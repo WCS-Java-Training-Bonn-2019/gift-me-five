d7b6411c2ad44965ff3522cc8838f6c49366d330
Steffen Klaski <steffen.klaski@t-systems.com>
2020-02-12 14:12:42 +0100

    new Wishlist + delete Wishlist working

--- src/main/java/com/gift_me_five/GiftMeFive.java
+++ src/main/java/com/gift_me_five/GiftMeFive.java
@@ -11,19 +11,38 @@ public class GiftMeFive {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GiftMeFive.class, args);
-		
+
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	public static void debugOut(Object message) {
-		System.out.println();System.out.println();
+		System.out.println();
+		System.out.println();
 		System.out.println("*".repeat(120));
 		System.out.println(message);
 		System.out.println("*".repeat(120));
-		System.out.println();System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+	public static void debugOut(Object message, int i) {
+		System.out.println();
+		System.out.println();
+		System.out.println("*".repeat(120));
+		System.out.println(message);
+		System.out.println("*".repeat(120));
+		System.out.println();
+		System.out.println();
+
+		try {
+			Thread.sleep(i * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/gift_me_five/controller/WishlistController.java
+++ src/main/java/com/gift_me_five/controller/WishlistController.java
@@ -12,12 +12,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.gift_me_five.GiftMeFive;
+import com.gift_me_five.entity.User;
 import com.gift_me_five.entity.Wish;
 import com.gift_me_five.entity.Wishlist;
 import com.gift_me_five.repository.ThemeRepository;
+import com.gift_me_five.repository.UserRepository;
 import com.gift_me_five.repository.WishRepository;
 import com.gift_me_five.repository.WishlistRepository;
 import com.gift_me_five.service.UserArtifactsService;
@@ -27,6 +31,9 @@ public class WishlistController {
 
 	@Autowired
 	private WishlistRepository wishlistRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Autowired
 	private ThemeRepository themeRepository;
@@ -46,16 +53,16 @@ public class WishlistController {
 		if (wishlist != null) {
 			model.addAttribute("myUserId", userArtifactsService.getCurrentUser().getId());
 			// Flag to indicate whether wishes should be sorted by price
-            model.addAttribute("sort", sort);
+			model.addAttribute("sort", sort);
 			// Flag to indicate whether wishes selected by other friends shall be hidden
-            model.addAttribute("hide", hide);
-            // Populate menu item for own wishlists (titles needed)
+			model.addAttribute("hide", hide);
+			// Populate menu item for own wishlists (titles needed)
 			model.addAttribute("myWishlists", userArtifactsService.allOwnWishlists());
-			// Populate menu item for friends wishlists  (titles needed)
+			// Populate menu item for friends wishlists (titles needed)
 			model.addAttribute("friendWishlists", userArtifactsService.allFriendWishlists());
 			// Title, id and theme of current wishlist needed to build the page:
 			model.addAttribute("wishlist", wishlist);
-			//model.addAttribute("wishes", wishRepository.findByWishlist(wishlist));
+			// model.addAttribute("wishes", wishRepository.findByWishlist(wishlist));
 			model.addAttribute("wishes", userArtifactsService.unSelectedWishes(wishlist, sort));
 			return "giver";
 		}
@@ -67,9 +74,9 @@ public class WishlistController {
 
 	@PostMapping("/giver")
 	public String updateWish(@RequestParam(required = false) Long wishId, @RequestParam(required = false) boolean hide,
-			 @RequestParam(required = false) boolean sort) {
+			@RequestParam(required = false) boolean sort) {
 		// System.out.println("Wish ID = " + wishId);
-		
+
 		Wish wish = userArtifactsService.friendWish(wishId);
 		if (wish != null) {
 			Wishlist wishlist = wish.getWishlist();
@@ -87,9 +94,42 @@ public class WishlistController {
 		return "redirect:/under_construction";
 	}
 
+	@GetMapping({ "/public/receiver", "/public/receiver/{uniqueUrlReceiver}" })
+	public String displayPublicWishlist(Principal principal, Model model, HttpServletRequest request,
+			@PathVariable(required = false) String uniqueUrlReceiver) {
+		
+		// only Anonymous user should be here
+		if (principal != null) {
+			return "redirect:/";
+		}
+
+		model.addAttribute("visibility", "public");
+		Wishlist wishlist = new Wishlist();
+		
+		if (uniqueUrlReceiver != null && wishlistRepository.findByUniqueUrlReceiver(uniqueUrlReceiver).getReceiver().getId() == 2) {
+			wishlist = wishlistRepository.findByUniqueUrlReceiver(uniqueUrlReceiver);
+		} else {
+			return "redirect:/public/wishlist";
+		}
+
+		model.addAttribute("thisWishlistId", wishlist.getId());
+		model.addAttribute("wishlist", wishlist);
+		model.addAttribute("wishes", wishRepository.findByWishlist(wishlist));
+		
+		model.addAttribute("thisWishlistId", wishlist.getId());
+		model.addAttribute("wishlist", wishlist);
+		model.addAttribute("myWishlists", userArtifactsService.allOwnWishlists());
+		model.addAttribute("friendWishlists", userArtifactsService.allFriendWishlists());
+		model.addAttribute("wishes", wishRepository.findByWishlist(wishlist));
+		// todo: add protocol to model (invite url, receiver.html)
+		model.addAttribute("hostname", request.getLocalName());
+		model.addAttribute("port", request.getLocalPort());
+		return ("receiver");
+	}
+
 	@GetMapping("/receiver")
 	public String displayWishlist(Model model, @RequestParam(required = false) Long id, HttpServletRequest request) {
-	
+
 //		hostname (e.g. localhost)
 //		GiftMeFive.debugOut(request.getLocalName());
 //		ip address (e.g. 127.0.0.1)
@@ -111,6 +151,33 @@ public class WishlistController {
 		model.addAttribute("hostname", request.getLocalName());
 		model.addAttribute("port", request.getLocalPort());
 		return ("receiver");
+	}
+
+	@GetMapping({ "/public/wishlist", "/public/wishlist/{uniqueUrlReceiver}" })
+	public String newPublicWishlist(Model model, Principal principal,
+			@PathVariable(required = false) String uniqueUrlReceiver) {
+		// only Anonymous user should be here
+		if (principal != null) {
+			return "redirect:/wishlist";
+		}
+
+		model.addAttribute("visibility", "public");
+		model.addAttribute("themes", themeRepository.findAll());
+
+		if (uniqueUrlReceiver != null
+				&& wishlistRepository.findByUniqueUrlReceiver(uniqueUrlReceiver).getReceiver().getId() == 2) {
+			// edit wishlist uniqueUrlReceiver
+			Wishlist wishlist = wishlistRepository.findByUniqueUrlReceiver(uniqueUrlReceiver);
+			model.addAttribute("wishlist", wishlist);
+			return "wishlist";
+		}
+
+		// new public wishlist
+		Wishlist wishlist = new Wishlist();
+		wishlist.setReceiver(userRepository.findById(2L).get());
+		wishlist.setTheme(themeRepository.findById(1L).get()); // default theme set
+		model.addAttribute("wishlist", wishlist);
+		return "wishlist";
 	}
 
 	@GetMapping({ "/wishlist", "/newwishlist" })
@@ -141,28 +208,33 @@ public class WishlistController {
 		return "wishlist";
 	}
 
-	@PostMapping("/wishlist")
+	@PostMapping({ "/wishlist", "/public/wishlist" })
 	public String saveWishList(@ModelAttribute Wishlist wishlist, @RequestParam("submit") String submit) {
 
 		if (wishlist.getId() == null) {
-			
-			wishlist.setReceiver(userArtifactsService.getCurrentUser());
-			
-			//if null or empty, create UUID as uniqueUrlReceiver
+
+//			wishlist.setReceiver(userArtifactsService.getCurrentUser());
+			User current = userArtifactsService.getCurrentUser();
+			if (current == null) { // no current, so anonymous -> default public user
+				current = userRepository.findById(2L).get();
+			}
+//				wishlist.setReceiver(userArtifactsService.getCurrentUser());
+			wishlist.setReceiver(current);
+			// if null or empty, create UUID as uniqueUrlReceiver
 			if (wishlist.getUniqueUrlReceiver() == null || wishlist.getUniqueUrlReceiver().isEmpty()) {
 				String uniqueUrlReceiver;
 				do {
-				uniqueUrlReceiver = UUID.randomUUID().toString();
-				} while (wishlistRepository.findByUniqueUrlReceiver(uniqueUrlReceiver) != null); 
+					uniqueUrlReceiver = UUID.randomUUID().toString();
+				} while (wishlistRepository.findByUniqueUrlReceiver(uniqueUrlReceiver) != null);
 				wishlist.setUniqueUrlReceiver(uniqueUrlReceiver);
 			}
 
-			//if null or empty, create UUID as uniqueUrlGiver
+			// if null or empty, create UUID as uniqueUrlGiver
 			if (wishlist.getUniqueUrlGiver() == null || wishlist.getUniqueUrlGiver().isEmpty()) {
 				String uniqueUrlGiver;
 				do {
-				uniqueUrlGiver = UUID.randomUUID().toString();
-				} while (wishlistRepository.findByUniqueUrlReceiver(uniqueUrlGiver) != null); 
+					uniqueUrlGiver = UUID.randomUUID().toString();
+				} while (wishlistRepository.findByUniqueUrlReceiver(uniqueUrlGiver) != null);
 				wishlist.setUniqueUrlGiver(uniqueUrlGiver);
 			}
 
@@ -170,30 +242,36 @@ public class WishlistController {
 		} else {
 			// Check if own wishlist; only then modify.
 			// Only take over title and theme
+			// unless public
 			Wishlist myWishlist = userArtifactsService.ownWishlist(wishlist.getId());
+
+			if (myWishlist == null  ) {
+				myWishlist = wishlistRepository.findById(wishlist.getId()).get();
+			}
+
 			if (myWishlist != null) {
 				myWishlist.setTheme(wishlist.getTheme());
 				myWishlist.setTitle(wishlist.getTitle());
 				wishlist = myWishlist;
-				
-				//if null or empty, create UUID as uniqueUrlReceiver
+
+				// if null or empty, create UUID as uniqueUrlReceiver
 				if (wishlist.getUniqueUrlReceiver() == null || wishlist.getUniqueUrlReceiver().isEmpty()) {
 					String uniqueUrlReceiver;
 					do {
-					uniqueUrlReceiver = UUID.randomUUID().toString();
-					} while (wishlistRepository.findByUniqueUrlReceiver(uniqueUrlReceiver) != null); 
+						uniqueUrlReceiver = UUID.randomUUID().toString();
+					} while (wishlistRepository.findByUniqueUrlReceiver(uniqueUrlReceiver) != null);
 					wishlist.setUniqueUrlReceiver(uniqueUrlReceiver);
-				}				
+				}
 
-				//if null or empty, create UUID as uniqueUrlGiver
+				// if null or empty, create UUID as uniqueUrlGiver
 				if (wishlist.getUniqueUrlGiver() == null || wishlist.getUniqueUrlGiver().isEmpty()) {
 					String uniqueUrlGiver;
 					do {
-					uniqueUrlGiver = UUID.randomUUID().toString();
-					} while (wishlistRepository.findByUniqueUrlReceiver(uniqueUrlGiver) != null); 
+						uniqueUrlGiver = UUID.randomUUID().toString();
+					} while (wishlistRepository.findByUniqueUrlReceiver(uniqueUrlGiver) != null);
 					wishlist.setUniqueUrlGiver(uniqueUrlGiver);
 				}
-				
+
 				wishlistRepository.save(wishlist);
 			}
 		}
@@ -202,13 +280,18 @@ public class WishlistController {
 			return "redirect:/wish?wishlistId=" + wishlist.getId();
 		}
 
-		return "redirect:/receiver?id=" + wishlist.getId();
+		if (wishlist.getReceiver().getId() != 2) {
+			return "redirect:/receiver?id=" + wishlist.getId();
+		} else {
+			return "redirect:/public/receiver/" + wishlist.getUniqueUrlReceiver();
+		}
 	}
 
-	@GetMapping("/wishlist/delete")
+	@GetMapping({"/wishlist/delete", "/public/wishlist/delete"})
 	public String deleteWishList(@RequestParam Long id) {
 		// Check if own wishlist - otherwise don't delete
-		if (userArtifactsService.ownWishlist(id) != null) {
+		// unless its public
+		if (userArtifactsService.ownWishlist(id) != null || wishlistRepository.findById(id).get().getReceiver().getId() == 2) {
 			wishlistRepository.deleteById(id);
 		}
 		// redirect to another own wishlist if exists,
